b9ccd025a16c1a2d704b3293566cb5c24679e0a8
taraokelly <taraokelly.dev@gmail.com>
2017-03-26 20:22:26 +0100

    Add Slides to Sign Up Page and Sorted Issue with Input Boxes

--- groupd-mobile-application/src/pages/signup/signup.ts
+++ groupd-mobile-application/src/pages/signup/signup.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-signup',
@@ -7,10 +9,53 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  @ViewChild(Slides) slides: Slides;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+
+  user: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.user = {
+      email: "",
+      username: "",
+      password: "",
+      firstname: "",
+      surname: "",
+      job: "",
+      bio: ""
+    }
+  }
+
+ goToSlide(index) {
+    this.slides.slideTo(index, 500);
+  }
+
+  addUser(){
+  /* this.ContactData.addUserProvider(JSON.stringify(this.user))
+      .subscribe(
+        data => alert("Successful!"),
+        err => alert("Unsuccessful!" + err),
+        () => console.log("Finished")
+      );*/
+    this.user = {
+      email: "",
+      username: "",
+      password: "",
+      firstname: "",
+      surname: "",
+      job: "",
+      bio: ""
+    }
   }
 
 }
+interface User{
+  email: string;
+  username: string;
+  password: string;
+  firstname: string;
+  surname: string;
+  job: string;
+  bio: string;
+}
+
